56dc484502277a274d1103568ab36ed775824ca4
norrii1 <alexnoriega47@gmail.com>
2021-07-20 14:28:13 -0700

    GeoLocation

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 33.8919157,
+      lng: -118.13325100000002
+    },
+    zoom: 15
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCzfVue49sMcwHHa1FXAYDiSrpE1CTJ6IE' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+
+    );
+  }
+}
+
+export default SimpleMap;
